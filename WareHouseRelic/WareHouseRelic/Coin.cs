e457912b55dd39b53420efac70336a0a4acfcc6a
isaew <comandor.cry@yandex.ru>
2015-06-15 20:37:12 +0300

    Исправление предыдущего комита

---
+++ WareHouseRelic/WareHouseRelic/Coin.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WareHouseRelic
+{
+    /// <summary>
+    /// Создание и манитуляция записями о монетах
+    /// </summary>
+    class Coin
+    {
+        #region Конструктор класа
+        /// <summary>
+        /// Параметризированый конструктор класа Coin
+        /// </summary>
+        /// <param name="name">Название монеты</param>
+        /// <param name="age">Год чеканки</param>
+        /// <param name="typeOfMetal">Метал монеты</param>
+        /// <param name="letters">Буквенное обозначение монетного двора</param>
+        public Coin(string name, int year, string typeOfMetal, string letters)
+        {
+            Name = name;
+            Year = year;
+            TypeOfMetal = typeOfMetal;
+            Letters = letters;
+        }
+
+        /// <summary>
+        /// Пустой конструктор экземпляра класа Coin
+        /// </summary>
+        public Coin()
+        {
+
+        }
+        #endregion
+
+        #region Свойства класа
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+
+        public string TypeOfMetal { get; set; }
+
+        public string Letters { get; set; }
+        #endregion
+
+        #region Методы класа
+        /// <summary>
+        /// Тестовое подключение к БД
+        /// </summary>
+        public void TestConnectToDb()
+        {
+            string connectionString = Properties.Settings.Default.DatabaseConnectionString;
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = connectionString;
+            cn.Open();
+            System.Windows.Forms.MessageBox.Show("Тестовое подключение к базе данных успешно");
+            cn.Close();
+        }
+        /// <summary>
+        /// Добавление новой записи о монете
+        /// </summary>
+        /// <param name="name">Название монеты</param>
+        /// <param name="age">Год чеканки</param>
+        /// <param name="typeOfMetal">Метал монеты</param>
+        /// <param name="letters">Буквенное обозначение монетного двора</param>
+        public void AddNewCoin(string name, int age, string typeOfMetal, string letters, string queryString)
+        {
+            string connectionString = Properties.Settings.Default.DatabaseConnectionString;
+
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(queryString, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+        /// <summary>
+        /// Редактирование записи о экспонате
+        /// </summary>
+        /// <param name="name">Название монеты</param>
+        /// <param name="age">Год чеканки</param>
+        /// <param name="typeOfMetal">Метал монеты</param>
+        /// <param name="letters">Буквенное обозначение монетного двора</param>
+        public void EditCoin(string name, int age, string typeOfMetal, string letters)
+        {
+            throw new NotImplementedException();
+        }
+        /// <summary>
+        /// Удаление записи о монете
+        /// </summary>
+        public void DeleteCoin()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Вывод базы данных в ListVeiw
+        /// </summary>
+        /// <param name="lv">Экземпляр ListVeiw в который будет происходить вывод базы данных</param>
+        /// <param name="queryString">SQL - запрос на получение данных</param>
+        public void DataOutput(ListView lv, string queryString)
+        {
+            lv.Items.Clear();
+            string connectionString = Properties.Settings.Default.DatabaseConnectionString;
+
+            SqlConnection con = new SqlConnection(connectionString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(queryString, con);
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                string[] sss = new string[4];
+                while (dr.Read())
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        sss[j] = Convert.ToString(dr[j + 1]);
+                    }
+
+                    ListViewItem lvi = new ListViewItem(sss);
+                    lv.Items.Add(lvi);
+                }
+            }
+            con.Close();
+        }
+        #endregion
+    }
+}
---
+++ WareHouseRelic/WareHouseRelic/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WareHouseRelic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            treeView1.TopNode.Expand(); //Розвертывание первого узла
+
+            Coin c = new Coin();
+            c.TestConnectToDb();
+            c.DataOutput(listView1, "SELECT * FROM CoinTable");
+            c.AddNewCoin("qwerty", 2000, "ytreq", "qazxsw", "INSERT INTO CoinTable (ID, NameCoin) values('6', 'Rublшл')");
+            c.DataOutput(listView1, "SELECT * FROM CoinTable");
+        }
+
+        #region Обработчики строки меню
+        private void параметрыСтраницыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void предваToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void печатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void вToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void экспортВPDFToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void экспортВExcelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void экспортВHTMLToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void создатьРезевкуюКопиюToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void востановитьРезервнуюКопиюToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void статистикаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void настройкиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void просмотрСправкиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void отправитьОтзывToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+    }
+}
