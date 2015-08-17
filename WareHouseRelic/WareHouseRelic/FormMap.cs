ee6f25ed9a9af3f09e4e102746ed14cbf1c97917
isaew <comandor.cry@yandex.ru>
2015-08-17 14:04:11 +0300

    ф

---
+++ WareHouseRelic/WareHouseRelic/FormMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WareHouseRelic
+{
+    public partial class FormMap : Form
+    {
+        public FormMap()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMap_Load(object sender, EventArgs e)
+        {
+            gMapControl1.MapProvider = GMap.NET.MapProviders.GMapProviders.GoogleMap;
+            gMapControl1.Position = new GMap.NET.PointLatLng(Properties.Settings.Default.PositionLat, Properties.Settings.Default.PositionLng);
+            gMapControl1.DragButton = MouseButtons.Left;
+            GMap.NET.GMaps.Instance.Mode = GMap.NET.AccessMode.ServerOnly;
+        }
+    }
+}
